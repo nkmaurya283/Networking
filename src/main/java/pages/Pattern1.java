15190343d77c9f9de7ba48333bcb5438950acac8
nkmaurya283 <nkmaurya283@gmail.com>
2018-07-07 16:19:25 +0530

    pushing

---
+++ src/main/java/pages/Pattern1.java
@@ -0,0 +1,21 @@
+package pages;
+
+public class Pattern1 {
+
+   //String x="12345";
+   // String y=null;
+
+
+    public  static  void  main(String[] args){
+
+        for(int i=1; i<=5; i++)
+        {
+            System.out.println("*");
+            
+            for(int j=2; j<i; i++)
+            {System.out.println("1");}
+
+
+       }
+    }
+}
